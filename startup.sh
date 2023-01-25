10b2d7068ec68a5d30f49f4b4830208ea94e568f
jcoker85 <jamescoker91@gmail.com>
2023-01-25 14:38:02 -0600

    More permissions fixes

--- startup.sh
+++ startup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+#Thanks to ich777 for a lot of the content in this script
+
+echo "---Ensuring UID: 99 matches user---"
+usermod -u 99 steam
+echo "---Ensuring GID: 100 matches user---"
+groupmod -g 100 steam > /dev/null 2>&1 ||:
+usermod -g 100 steam
+echo "---Setting umask to 000---"
+umask 000
+
+echo "---Taking ownership of data...---"
+chown -R 99:100 /servers/geserver
+
 if [ ! -e /servers/geserver/srcds.exe ]; then
   echo "---Installing Source SDK 2007---"
   /steamcmd/steamcmd.sh +@sSteamCmdForcePlatformType windows +force_install_dir /servers/geserver +logon anonymous +app_update 310 +quit
@@ -13,7 +26,6 @@ else
   echo "---Goldeneye Source Server found---"
 fi
 
-#Thanks to ich777 for this Wine config
 export WINEARCH=win64
 export WINEPREFIX=/servers/geserver/WINE
 echo "---Checking if WINE workdirectory is present---"
